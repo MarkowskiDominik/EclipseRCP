7c1f35aaf1b6ac83a5cfb69085ec7055b7cf0768
Dominik Markowski <markowski.dominik.j@gmail.com>
2016-04-19 09:50:45 +0200

    add lend history list

--- src/markowski/library/view/BookDetailsView.java
+++ src/markowski/library/view/BookDetailsView.java
@@ -1,29 +1,26 @@
 package markowski.library.view;
 
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 
 import markowski.library.data.model.Book;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Combo;
 
 public class BookDetailsView extends ViewPart {
 
-	private Table table;
-	private ISelectionListener selectionListerner;
 	private Text bookDetails;
+	private ISelectionListener selectionListerner;
+	private TableViewer lendTableViewer;
 
 	public BookDetailsView() {
 		// TODO Auto-generated constructor stub
@@ -37,16 +34,12 @@ public class BookDetailsView extends ViewPart {
 		bookDetails.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		bookDetails.setEditable(false);
 		
-		TableViewer tableViewerLend = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
-		table = tableViewerLend.getTable();
-		GridData gd_table = new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1);
-		gd_table.widthHint = 350;
-		table.setLayoutData(gd_table);
-		
-		TableViewerColumn tableViewerColumn = new TableViewerColumn(tableViewerLend, SWT.NONE);
-		TableColumn tableLend = tableViewerColumn.getColumn();
-		tableLend.setWidth(350);
-		tableLend.setText("Lend");
+		lendTableViewer = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
+		lendTableViewer.setContentProvider(ArrayContentProvider.getInstance());
+		Table lendTable = lendTableViewer.getTable();
+		GridData gd_lendTable = new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1);
+		gd_lendTable.widthHint = 350;
+		lendTable.setLayoutData(gd_lendTable);
 		
 		selectionListerner = new ISelectionListener() {
 			@Override
@@ -54,16 +47,12 @@ public class BookDetailsView extends ViewPart {
 				if (!(selection instanceof IStructuredSelection)) {
 					return;
 				}
-				IStructuredSelection structuredSelection = (IStructuredSelection) selection;
-				Object object = structuredSelection.getFirstElement();
+				Object object = ((IStructuredSelection) selection).getFirstElement();
 				if (object instanceof Book) {
 					Book book = (Book) object;
 					String details = "\"" + book.getTitle() + "\", " + book.getAuthors();
 					bookDetails.setText(details);
-					tableLend.setData(book.getLendHistory());
-					
-					System.out.println(book.getLendHistory().size());
-					System.out.println(book.getLendHistory().get(0));
+					lendTableViewer.setInput(book.getLendHistory());
 				}
 			}
 
@@ -71,11 +60,9 @@ public class BookDetailsView extends ViewPart {
 		getSite().getPage().addSelectionListener(selectionListerner);
 	}
 
-
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
 
 	}
-
 }
